5d9a2e7b9106a8a915bc3b5aa9eb1f35dbd733a9
Deepan Sankar <deephanmail2u@gmail.com>
2021-01-21 19:13:49 +0530

    changes

---
+++ Django/cloneOne/appOne/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url, include
+from . import views
+
+urlpatterns = [
+    url(r'^$',views.views.PostListView(),name='post_list'),
+    url(r'^about/$',views.AboutView.as_view(),name='about'),
+    url(r'^post/(?P<pk>\d+)$',views.PostDetailView.as_view(),name='post_detail'),
+    url(r'^post/new/$',views.CreatePostView.as_view(),name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.PostUpdateView.as_view(), name='post_edit'),
+    url(r'^post/(?P<pk>\d+)/delete/$',views.PostDeleteView.as_view(),name='post_delete'),
+    url(r'^drafts/$',views.DraftListView.as_view(),name='post_drafts'),
+    url(r'^post/(?P<pk>\d+)/comment/$',views.add_comments_to_post,name='add_comment_to_post'),
+    url(r'^comments/(?P<pk>\d+)/approve/$',views.comment_approved,name='comment_approve'),
+    url(r'^comment/(?P<pk>\d+)/remove/$',views.views.comment_remove,name='commnt_remove'),
+    url(r'^post/(?P<pk>\d+)/publish/$',views.post_publist,name='post_publish'),
+]
