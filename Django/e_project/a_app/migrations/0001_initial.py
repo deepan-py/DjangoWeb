be590d9b11b542724a0b1619470b6af74fca9199
Deepan Sankar <deephanmail2u@gmail.com>
2021-01-26 17:23:55 +0530

    Changed

--- Django/e_project/a_app/migrations/0001_initial.py
+++ Django/e_project/a_app/migrations/0001_initial.py
@@ -19,7 +19,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('portfolio_site', models.URLField(blank=True)),
                 ('profile_pic', models.ImageField(blank=True, upload_to='profile_pics')),
-                ('user', models.OneToOneField(on_delete=None, to=settings.AUTH_USER_MODEL)),
+                ('user', models.OneToOneField(on_delete=models.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
     ]
