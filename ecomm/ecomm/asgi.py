c8ad131e8a4f45c27a2abcc0937e6af4c8270a18
Deepan Sankar <deephanmail2u@gmail.com>
2021-02-13 20:46:15 +0530

    Added Ecomm

--- ecomm/ecomm/asgi.py
+++ ecomm/ecomm/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ecommerce project.
+ASGI config for ecomm project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecomm.settings')
 
 application = get_asgi_application()
